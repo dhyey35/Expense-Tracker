d68e6774df1216a6485a8603a5f1645af7747bdd
dhyey35 <dhyey35@gmailcom>
2015-09-14 12:52:12 +0530

    Add color

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
-//add a paid/delete button
+
 //add comments
 // after deleting add them to a file
+session_start();
+print "<h2 style='color:#2e8ae6;'> Welcome ".$_SESSION['username']."</h2>";
 require_once('expense.php');
-print "<hr>";
+print "<hr color=#22b7f6>";
 require_once('income.php');
-print "<hr>";
+print "<hr color=#22b7f6>";
 require_once('creditors.php');
-print "<hr>";
+print "<hr color=#22b7f6>";
 require_once('debtors.php');
-print "<hr>";
+print "<hr color=#22b7f6>";
 ?>
