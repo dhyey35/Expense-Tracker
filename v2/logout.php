802e674a85bb9af5b6ff804c7b81f621b0eaf1f2
dhyey35 <dhyey35@gmailcom>
2016-05-22 20:23:34 +0530

    redirect directly to login.php

--- v2/logout.php
+++ v2/logout.php
@@ -11,5 +11,7 @@ if(isset($_COOKIE['username'])) {
 if(isset($_COOKIE['email'])) {
 	setcookie('email','',time()-30);
 }
+//change
+header("Location: http://localhost/expense/v2/login.php");
 print "<strong style='font-size:1em;'>You have been successfully logged out <a href=login.php >Log In Again</a></strong>";
 ?>
